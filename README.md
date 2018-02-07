5a57a7761eb76bc71640d416004cd05c03825051
jasjuang <jasjuang@gmail.com>
2018-02-07 10:42:55 -0800

    add in travis, install message, fix coding style

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-CMake Format
+Sublime CMake Format
 ============
+
+[![Build Status](https://travis-ci.org/jasjuang/sublime_cmake_format.svg?branch=master)](https://travis-ci.org/jasjuang/sublime_cmake_format)
 
 What it does
 ------------
@@ -9,21 +11,24 @@ package that allows you to run it easily from within Sublime Text.
 Installing
 ----------
 
-- This plugin is dependent on the [cmake_format](https://github.com/cheshirekow/cmake_format)
-  and the [Sublime CMake](https://packagecontrol.io/packages/CMake) plugin.
+- This plugin is dependent on [cmake_format](https://github.com/cheshirekow/cmake_format)
+  and [Sublime CMake](https://packagecontrol.io/packages/CMake) plugin.
 
-- Please install cmake_format by 
+To install `cmake_format`, do the following:
 
-```
-sudo pip install cmake_format
-```
+1. Install [Python](http://python.org/download/) and [pip](http://www.pip-installer.org/en/latest/installing.html).
+
+2. Install `cmake_format` by typing the following in a terminal:
+   ```
+   [sudo] pip install cmake_format
+   ```
 
 - Please install Sublime CMake and this package through Package Control in the 
   usual way.
 
 - Set the path to the cmake-format binaries. You can do this from within Sublime
   Text by choosing `CMake Format - Set Path` from the command palette.  Hint:
-  the path should look something like this `[path/to/cmake]/cmake/bin/cmake-format`.
+  the path should look something like this `/usr/local/bin/cmake-format`.
   If cmake-format is in your system path, you shouldn't need to do anything.
 
 Use
@@ -51,7 +56,12 @@ If You Liked This
 -----------------
 - ... And want to contribute, PR's gladly accepted!
 
-- Otherwise, why not pop on over and star this repo on GitHub?
+Please note that modifications should follow these coding guidelines:
+
+- Indent is 4 spaces.
+- Code should pass flake8 and pep257 linters.
+- Vertical whitespace helps readability, don’t be afraid to use it.
+- Please use descriptive variable names, no abbreviations unless they are very well known.
 
 Credits
 -------
--- cmake_format.py
+++ cmake_format.py
@@ -1,47 +1,61 @@
-import sublime, sublime_plugin
-import subprocess, os
+#
+# cmake_format.py
+# Autoformatter for CMakefiles
+#
+# Written by Jason Juang,,,
+# Copyright (c) 2018 Jason Juang,,,
+#
+# License: MIT
+#
+
+"""This module applies cmake_format in sublime."""
+
+import sublime
+import sublime_plugin
+import subprocess
+import os
 
 # Settings file locations.
 settings_file = 'cmake_format.sublime-settings'
 custom_style_settings = 'cmake_format_custom.sublime-settings'
 
 st_encodings_trans = {
-   "UTF-8" : "utf-8",
-   "UTF-8 with BOM" : "utf-8-sig",
-   "UTF-16 LE" : "utf-16-le",
-   "UTF-16 LE with BOM" : "utf-16",
-   "UTF-16 BE" : "utf-16-be",
-   "UTF-16 BE with BOM" : "utf-16",
-   "Western (Windows 1252)" : "cp1252",
-   "Western (ISO 8859-1)" : "iso8859-1",
-   "Western (ISO 8859-3)" : "iso8859-3",
-   "Western (ISO 8859-15)" : "iso8859-15",
-   "Western (Mac Roman)" : "mac-roman",
-   "DOS (CP 437)" : "cp437",
-   "Arabic (Windows 1256)" : "cp1256",
-   "Arabic (ISO 8859-6)" : "iso8859-6",
-   "Baltic (Windows 1257)" : "cp1257",
-   "Baltic (ISO 8859-4)" : "iso8859-4",
-   "Celtic (ISO 8859-14)" : "iso8859-14",
-   "Central European (Windows 1250)" : "cp1250",
-   "Central European (ISO 8859-2)" : "iso8859-2",
-   "Cyrillic (Windows 1251)" : "cp1251",
-   "Cyrillic (Windows 866)" : "cp866",
-   "Cyrillic (ISO 8859-5)" : "iso8859-5",
-   "Cyrillic (KOI8-R)" : "koi8-r",
-   "Cyrillic (KOI8-U)" : "koi8-u",
-   "Estonian (ISO 8859-13)" : "iso8859-13",
-   "Greek (Windows 1253)" : "cp1253",
-   "Greek (ISO 8859-7)" : "iso8859-7",
-   "Hebrew (Windows 1255)" : "cp1255",
-   "Hebrew (ISO 8859-8)" : "iso8859-8",
-   "Nordic (ISO 8859-10)" : "iso8859-10",
-   "Romanian (ISO 8859-16)" : "iso8859-16",
-   "Turkish (Windows 1254)" : "cp1254",
-   "Turkish (ISO 8859-9)" : "iso8859-9",
-   "Vietnamese (Windows 1258)" :  "cp1258",
-   "Hexadecimal" : None,
-   "Undefined" : None
+   "UTF-8": "utf-8",
+   "UTF-8 with BOM": "utf-8-sig",
+   "UTF-16 LE": "utf-16-le",
+   "UTF-16 LE with BOM": "utf-16",
+   "UTF-16 BE": "utf-16-be",
+   "UTF-16 BE with BOM": "utf-16",
+   "Western (Windows 1252)": "cp1252",
+   "Western (ISO 8859-1)": "iso8859-1",
+   "Western (ISO 8859-3)": "iso8859-3",
+   "Western (ISO 8859-15)": "iso8859-15",
+   "Western (Mac Roman)": "mac-roman",
+   "DOS (CP 437)": "cp437",
+   "Arabic (Windows 1256)": "cp1256",
+   "Arabic (ISO 8859-6)": "iso8859-6",
+   "Baltic (Windows 1257)": "cp1257",
+   "Baltic (ISO 8859-4)": "iso8859-4",
+   "Celtic (ISO 8859-14)": "iso8859-14",
+   "Central European (Windows 1250)": "cp1250",
+   "Central European (ISO 8859-2)": "iso8859-2",
+   "Cyrillic (Windows 1251)": "cp1251",
+   "Cyrillic (Windows 866)": "cp866",
+   "Cyrillic (ISO 8859-5)": "iso8859-5",
+   "Cyrillic (KOI8-R)": "koi8-r",
+   "Cyrillic (KOI8-U)": "koi8-u",
+   "Estonian (ISO 8859-13)": "iso8859-13",
+   "Greek (Windows 1253)": "cp1253",
+   "Greek (ISO 8859-7)": "iso8859-7",
+   "Hebrew (Windows 1255)": "cp1255",
+   "Hebrew (ISO 8859-8)": "iso8859-8",
+   "Nordic (ISO 8859-10)": "iso8859-10",
+   "Romanian (ISO 8859-16)": "iso8859-16",
+   "Turkish (Windows 1254)": "cp1254",
+   "Turkish (ISO 8859-9)": "iso8859-9",
+   "Vietnamese (Windows 1258)":  "cp1258",
+   "Hexadecimal": None,
+   "Undefined": None
 }
 
 
@@ -53,9 +67,9 @@ os_is_windows = os.name == 'nt'
 default_binary = 'cmake-format'
 
 
-# This function taken from Stack Overflow response:
-# http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
 def which(program):
+    """http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python."""
+
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
     fpath, fname = os.path.split(program)
@@ -71,8 +85,9 @@ def which(program):
     return None
 
 
-# Set the path to the binary in the settings file.
 def set_path(path):
+    """Set the path to the binary in the settings file."""
+
     settings = sublime.load_settings(settings_file)
     settings.set('binary', path)
     sublime.save_settings(settings_file)
@@ -80,19 +95,21 @@ def set_path(path):
     load_settings()
 
 
-# Display input panel to update the path.
 def update_path():
+    """Display input panel to update the path."""
+
     load_settings()
     w = sublime.active_window()
     w.show_input_panel("Path to cmake-format: ", binary, set_path, None, None)
 
 
-# Check that the binary can be found and is executable.
 def check_binary():
+    """Check that the binary can be found and is executable."""
+
     # If we couldn't find the binary.
-    if (which(binary) == None):
+    if (which(binary) is None):
         # Try to guess the correct setting.
-        if (which(default_binary) != None):
+        if (which(default_binary) is not None):
             # Looks like cmake-format is in the path, remember that.
             set_path(default_binary)
             return True
@@ -106,26 +123,34 @@ def check_binary():
     return True
 
 
-# Load settings and put their values into global scope.
-# Probably a nicer way of doing this, but it's simple enough and it works fine.
 def load_settings():
+    """Load settings and put their values into global scope."""
+
     # We set these globals.
     global binary
     global format_on_save
     settings_global = sublime.load_settings(settings_file)
     settings_local = sublime.active_window().active_view().settings().get('CmakeFormat', {})
-    load = lambda name, default: settings_local.get(name, settings_global.get(name, default))
+
+    def load(name, default): return settings_local.get(name, settings_global.get(name, default))
     # Load settings, with defaults.
-    binary         = load('binary', default_binary)
+    binary = load('binary', default_binary)
     format_on_save = load('format_on_save', False)
 
+
 def is_supported(lang):
+    """Check whether the file is CMake related."""
+
     load_settings()
     return lang.endswith(('CMake' + '.tmLanguage', 'CMake' + '.sublime-syntax'))
 
-# Triggered when the user runs cmake format.
+
 class CmakeFormatCommand(sublime_plugin.TextCommand):
+    """Triggered when the user runs cmake format."""
+
     def run(self, edit, whole_buffer=False):
+        """Entry point for sublime to call this."""
+
         load_settings()
 
         if not check_binary():
@@ -145,9 +170,9 @@ class CmakeFormatCommand(sublime_plugin.TextCommand):
         if os_is_windows:
             startupinfo = subprocess.STARTUPINFO()
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        p   = subprocess.Popen(command, stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE, stdin=subprocess.PIPE,
-                               startupinfo=startupinfo)
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, stdin=subprocess.PIPE,
+                             startupinfo=startupinfo)
         output, error = p.communicate(buf.encode(encoding))
 
         # Display any errors returned by cmake-format using a message box,
@@ -176,9 +201,13 @@ class CmakeFormatCommand(sublime_plugin.TextCommand):
         # Re-enable previous tabs to space setting
         self.view.settings().set('translate_tabs_to_spaces', prev_tabs_to_spaces)
 
-# Hook for on-save event, to allow application of cmake-format on save.
+
 class CmakeFormatEventListener(sublime_plugin.EventListener):
+    """Hook for on-save event, to allow application of cmake-format on save."""
+
     def on_pre_save(self, view):
+        """Check syntax and format on save prior step."""
+
         # Only do this for supported languages
         syntax = view.settings().get('syntax')
         if is_supported(syntax):
@@ -191,5 +220,9 @@ class CmakeFormatEventListener(sublime_plugin.EventListener):
 
 # Called from the UI to update the path in the settings.
 class CmakeFormatSetPathCommand(sublime_plugin.WindowCommand):
+    """We can also update the path on the UI."""
+
     def run(self):
+        """Update the path."""
+
         update_path()
